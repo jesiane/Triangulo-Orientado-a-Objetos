80f083d5006017eceb303c68775290cb6bf4f3db
Jesiane Straubel <jesi1951@hotmail.com>
2023-04-06 13:24:57 -0300

    Add project files.

---
+++ Triangulo Orientado a Objetos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Triangulo_Orientado_a_Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulos triangulos = new Triangulos();
+
+            triangulos.ladoA = 7;
+            triangulos.ladoB = 8;
+            triangulos.ladoC = 9;
+
+
+            Console.WriteLine(triangulos.CalculoTriangulos());
+            
+        }
+    }
+}
---
+++ Triangulo Orientado a Objetos/Triangulos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo_Orientado_a_Objetos
+{
+    internal class Triangulos
+    {
+        public int ladoA;
+        public int ladoB;
+        public int ladoC;
+
+        public int CalculoTriangulos()
+        {
+            if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA)
+            {
+                Console.WriteLine("Os 3 lados formam um triangulo!\n");
+                if (ladoA == ladoB && ladoA == ladoC)
+                    Console.WriteLine("Equilatero\n");
+                else
+                    if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+                    Console.WriteLine("Isosceles\n");
+                else
+                    Console.WriteLine("Escaleno\n");
+            }
+            else
+                Console.WriteLine("Triangulo invalido!\n");
+
+            return 0;
+        }
+    }
+}
+
